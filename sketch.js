e56c5de107c1173c3803b25f1a330d5b56562b76
OseungKwon <00osos@naver.com>
2021-10-16 21:54:47 +0900

    Add camera toggle button

--- sketch.js
+++ sketch.js
@@ -2,41 +2,41 @@
 
 let canvas;
 
-let sketch = function(p){
-  p.setup = function(){
+let sketch = function (p) {
+  p.setup = function () {
     canvas = p.createCanvas(640, 480);
     canvas.id("canvas");
 
     p.colorMode(p.HSB);
-  }
+  };
 
-  p.draw = function(){
+  p.draw = function () {
     p.clear();
-    if(detections != undefined){
-      if(detections.multiHandLandmarks != undefined){
-          //p.drawHands();
-          // p.drawParts();
+    if (detections != undefined) {
+      if (detections.multiHandLandmarks != undefined) {
+        //p.drawHands();
+        // p.drawParts();
 
-          p.drawLines([0, 5, 9, 13, 17, 0]);//palm
-          p.drawLines([0, 1, 2, 3 ,4]);//thumb
-          p.drawLines([5, 6, 7, 8]);//index finger
-          p.drawLines([9, 10, 11, 12]);//middle finger
-          p.drawLines([13, 14, 15, 16]);//ring finger
-          p.drawLines([17, 18, 19, 20]);//pinky
+        p.drawLines([0, 5, 9, 13, 17, 0]); //palm
+        p.drawLines([0, 1, 2, 3, 4]); //thumb
+        p.drawLines([5, 6, 7, 8]); //index finger
+        p.drawLines([9, 10, 11, 12]); //middle finger
+        p.drawLines([13, 14, 15, 16]); //ring finger
+        p.drawLines([17, 18, 19, 20]); //pinky
 
-          p.drawLandmarks([0, 1], 0);//palm base
-          p.drawLandmarks([1, 5], 60);//thumb
-          p.drawLandmarks([5, 9], 120);//index finger
-          p.drawLandmarks([9, 13], 180);//middle finger
-          p.drawLandmarks([13, 17], 240);//ring finger
-          p.drawLandmarks([17, 21], 300);//pinky
+        p.drawLandmarks([0, 1], 0); //palm base
+        p.drawLandmarks([1, 5], 60); //thumb
+        p.drawLandmarks([5, 9], 120); //index finger
+        p.drawLandmarks([9, 13], 180); //middle finger
+        p.drawLandmarks([13, 17], 240); //ring finger
+        p.drawLandmarks([17, 21], 300); //pinky
       }
     }
-  }
+  };
 
-  p.drawHands = function(){
-    for(let i=0; i<detections.multiHandLandmarks.length; i++){
-      for(let j=0; j<detections.multiHandLandmarks[i].length; j++){
+  p.drawHands = function () {
+    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+      for (let j = 0; j < detections.multiHandLandmarks[i].length; j++) {
         let x = detections.multiHandLandmarks[i][j].x * p.width;
         let y = detections.multiHandLandmarks[i][j].y * p.height;
         let z = detections.multiHandLandmarks[i][j].z;
@@ -48,13 +48,13 @@ let sketch = function(p){
         p.point(x, y);
       }
     }
-  }
+  };
 
-  p.drawLandmarks = function(indexArray, hue){
+  p.drawLandmarks = function (indexArray, hue) {
     p.noFill();
     p.strokeWeight(8);
-    for(let i=0; i<detections.multiHandLandmarks.length; i++){
-      for(let j=indexArray[0]; j<indexArray[1]; j++){
+    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+      for (let j = indexArray[0]; j < indexArray[1]; j++) {
         let x = detections.multiHandLandmarks[i][j].x * p.width;
         let y = detections.multiHandLandmarks[i][j].y * p.height;
         // let z = detections.multiHandLandmarks[i][j].z;
@@ -62,24 +62,24 @@ let sketch = function(p){
         p.point(x, y);
       }
     }
-  }
+  };
 
-  p.drawLines = function(index){
+  p.drawLines = function (index) {
     p.stroke(0, 0, 255);
     p.strokeWeight(3);
-    for(let i=0; i<detections.multiHandLandmarks.length; i++){
-      for(let j=0; j<index.length-1; j++){
+    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
+      for (let j = 0; j < index.length - 1; j++) {
         let x = detections.multiHandLandmarks[i][index[j]].x * p.width;
         let y = detections.multiHandLandmarks[i][index[j]].y * p.height;
         // let z = detections.multiHandLandmarks[i][index[j]].z;
 
-        let _x = detections.multiHandLandmarks[i][index[j+1]].x * p.width;
-        let _y = detections.multiHandLandmarks[i][index[j+1]].y * p.height;
+        let _x = detections.multiHandLandmarks[i][index[j + 1]].x * p.width;
+        let _y = detections.multiHandLandmarks[i][index[j + 1]].y * p.height;
         // let _z = detections.multiHandLandmarks[i][index[j+1]].z;
         p.line(x, y, _x, _y);
       }
     }
-  }
-}
+  };
+};
 
 let myp5 = new p5(sketch);
