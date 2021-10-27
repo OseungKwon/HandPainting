2806e0a0948bf6e7d76d1f3e49c44c5078339970
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-10-27 13:08:00 +0900

    동작 인식 추가
    우리/식구/자고/나면
    주고받는/말

--- detection.js
+++ detection.js
@@ -6,7 +6,7 @@ const button = document
   .addEventListener("click", flipVideo);
 const hands = new Hands({
   locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3.1632795355/${file}`;
   },
 });
 
@@ -19,106 +19,149 @@ hands.setOptions({
 hands.onResults(gotHands);
 function gotHands(results) {
   detections = results;
-  console.log(detections);
-  //console.log(detections.multiHandLandmarks.length);
-  if (detections.multiHandLandmarks.length === 1) {
+  hand = detections.multiHandLandmarks;
+  //console.log(detections);
+  //console.log(hand.length);
+  if (hand.length === 1) {
     if (
       // z축과 동일(오른손만)
-      Math.abs(
-        detections.multiHandLandmarks[0][5].z -
-          detections.multiHandLandmarks[0][9].z
-      ) < 0.1 &&
-      Math.abs(
-        detections.multiHandLandmarks[0][4].z -
-          detections.multiHandLandmarks[0][8].z
-      ) < 0.1 &&
-      detections.multiHandLandmarks[0][16].y <
-        detections.multiHandLandmarks[0][13].y
+      Math.abs(hand[0][5].z - hand[0][9].z) < 0.1 &&
+      Math.abs(hand[0][4].z - hand[0][8].z) < 0.1 &&
+      hand[0][16].y < hand[0][13].y
     ) {
       console.log("참");
       document.getElementById("content").innerHTML = "참";
     }
     if (
       //검지만 펴져있는 손 모양(오른손만)
-      detections.multiHandLandmarks[0][12].y >
-        detections.multiHandLandmarks[0][9].y &&
-      detections.multiHandLandmarks[0][16].y >
-        detections.multiHandLandmarks[0][13].y &&
-      detections.multiHandLandmarks[0][20].y >
-        detections.multiHandLandmarks[0][17].y &&
-      detections.multiHandLandmarks[0][8].y <
-        detections.multiHandLandmarks[0][5].y
+      hand[0][12].y > hand[0][9].y &&
+      hand[0][16].y > hand[0][13].y &&
+      hand[0][20].y > hand[0][17].y &&
+      hand[0][8].y < hand[0][5].y
     ) {
       console.log("이 한마디");
       document.getElementById("content").innerHTML = "이 한마디";
       word = "이 한마디";
     }
     if (
-      // 주먹 쥔 모양(오른손만)
-      detections.multiHandLandmarks[0][12].y >
-        detections.multiHandLandmarks[0][9].y &&
-      detections.multiHandLandmarks[0][16].y >
-        detections.multiHandLandmarks[0][13].y &&
-      detections.multiHandLandmarks[0][20].y >
-        detections.multiHandLandmarks[0][17].y &&
-      detections.multiHandLandmarks[0][8].y >
-        detections.multiHandLandmarks[0][5].y
+      hand[0][5].x > hand[0][8].x &&
+      hand[0][9].x > hand[0][12].x &&
+      hand[0][13].x > hand[0][16].x &&
+      hand[0][1].x > hand[0][4].x
     ) {
-      console.log("좋은");
-      document.getElementById("content").innerHTML = "좋은";
+      document.getElementById("content").innerHTML = "우리";
     }
+
+    // if (
+    //   // 주먹 쥔 모양(오른손만)
+    //   hand[0][12].y >
+    //     hand[0][9].y &&
+    //   hand[0][16].y >
+    //     hand[0][13].y &&
+    //   hand[0][20].y >
+    //     hand[0][17].y &&
+    //   hand[0][8].y >
+    //     hand[0][5].y
+    // ) {
+    //   console.log("좋은");
+    //   document.getElementById("content").innerHTML = "좋은";
+    // }
   }
-  if (detections.multiHandLandmarks.length === 2) {
+  if (hand.length === 2) {
+    if (
+      hand[0][0].x < hand[0][12].x &&
+      hand[1][0].x > hand[1][12].x &&
+      hand[0][5].y > hand[0][8].y &&
+      hand[0][13].y > hand[0][16].y
+    ) {
+      console.log("식구");
+      document.getElementById("content").innerHTML = "식구";
+    }
     if (
       Math.abs(
         // 바닥과 수평된 손모양(오른손)
-        detections.multiHandLandmarks[0][4].y -
-          detections.multiHandLandmarks[0][20].y
+        hand[0][4].y - hand[0][20].y
       ) < 0.1 && // 주먹쥐고 있는 손 모양(왼손)
-      detections.multiHandLandmarks[1][10].x >
-        detections.multiHandLandmarks[1][12].x &&
-      detections.multiHandLandmarks[1][14].x >
-        detections.multiHandLandmarks[1][16].x &&
-      detections.multiHandLandmarks[1][18].x >
-        detections.multiHandLandmarks[1][20].x
+      hand[1][9].x < hand[1][10].x &&
+      hand[1][10].x > hand[1][12].x &&
+      hand[1][14].x > hand[1][16].x &&
+      hand[1][18].x > hand[1][20].x
     ) {
       console.log("사랑해요");
       document.getElementById("content").innerHTML = "사랑해요";
-      particle("love");
+      //particle("love");
     } else {
       //console.log("no");
     }
     if (
       //검지만 펴져있는 손 모양(오른손)
-      detections.multiHandLandmarks[0][12].y >
-        detections.multiHandLandmarks[0][9].y &&
-      detections.multiHandLandmarks[0][16].y >
-        detections.multiHandLandmarks[0][13].y &&
-      detections.multiHandLandmarks[0][20].y >
-        detections.multiHandLandmarks[0][17].y
+      hand[0][5].y > hand[0][8].y &&
+      hand[0][12].y > hand[0][9].y &&
+      hand[0][16].y > hand[0][13].y &&
+      hand[0][20].y > hand[0][17].y &&
+      hand[0][4].x < hand[0][3].x
     ) {
       if (
         //검지만 펴져있는 손 모양(왼손)
-        detections.multiHandLandmarks[1][12].y >
-          detections.multiHandLandmarks[1][9].y &&
-        detections.multiHandLandmarks[1][16].y >
-          detections.multiHandLandmarks[1][13].y &&
-        detections.multiHandLandmarks[1][20].y >
-          detections.multiHandLandmarks[1][17].y
+        hand[1][5].y > hand[1][8].y &&
+        hand[1][12].y > hand[1][9].y &&
+        hand[1][16].y > hand[1][13].y &&
+        hand[1][20].y > hand[1][17].y &&
+        hand[1][4].x > hand[1][3].x
       ) {
         console.log("말");
         document.getElementById("content").innerHTML = "말";
       }
     }
+    if (
+      hand[0][8].x > hand[0][0].x &&
+      hand[0][16].x > hand[0][13].x &&
+      hand[1][8].x > hand[1][0].x &&
+      hand[1][16].x > hand[1][13].x
+    ) {
+      console.log("자고");
+      document.getElementById("content").innerHTML = "자고";
+    }
+    if (
+      //검지만 펴져있는 손 모양(오른손)
+      hand[0][5].y > hand[0][8].y &&
+      hand[0][12].y > hand[0][9].y &&
+      hand[0][16].y > hand[0][13].y &&
+      hand[0][20].y > hand[0][17].y &&
+      hand[0][4].x > hand[0][3].x
+    ) {
+      if (
+        //검지만 펴져있는 손 모양(왼손)
+        hand[1][5].y > hand[1][8].y &&
+        hand[1][12].y > hand[1][9].y &&
+        hand[1][16].y > hand[1][13].y &&
+        hand[1][20].y > hand[1][17].y &&
+        hand[1][4].x < hand[1][3].x
+      ) {
+        console.log("나면");
+        document.getElementById("content").innerHTML = "나면";
+      }
+    }
+    if (
+      hand[0][4].x < hand[0][17].x &&
+      hand[0][0].z > hand[0][12].z &&
+      hand[1][0].z > hand[1][12].z &&
+      hand[0][2].z > hand[0][4].z &&
+      hand[0][5].y > hand[0][8].y &&
+      hand[0][13].y > hand[0][16].y
+    ) {
+      console.log("주고받는");
+      document.getElementById("content").innerHTML = "주고받는";
+    }
   }
-  // if (detections.multiHandLandmarks.length === 1) {
+  // if (hand.length === 1) {
   //   // love 손 모양
 
   //   if (
-  //     detections.multiHandLandmarks[0][12].y >
-  //       detections.multiHandLandmarks[0][9].y &&
-  //     detections.multiHandLandmarks[0][16].y >
-  //       detections.multiHandLandmarks[0][13].y
+  //     hand[0][12].y >
+  //       hand[0][9].y &&
+  //     hand[0][16].y >
+  //       hand[0][13].y
   //   ) {
   //     console.log("love");
   //   } else {
