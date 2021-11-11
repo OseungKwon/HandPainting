affa9650be703076896ddf26cd5fc8cc8e1a1991
OseungKwon <00osos@naver.com>
2021-11-12 04:56:51 +0900

    화면 캡쳐 기능 추가

--- song/detection.js
+++ song/detection.js
@@ -10,13 +10,13 @@ const button = document.querySelector(".button");
 const hands = new Hands({
   locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3.1632795355/${file}`;
-  },
+  }
 });
 
 hands.setOptions({
   maxNumHands: 2,
   minDetectionConfidence: 0.8,
-  minTrackingConfidence: 0.5,
+  minTrackingConfidence: 0.5
 });
 
 hands.onResults(gotHands);
@@ -244,7 +244,7 @@ const camera = new Camera(videoElement, {
     await hands.send({ image: videoElement });
   },
   width: 1920,
-  height: 1080,
+  height: 1080
 });
 camera.start();
 
