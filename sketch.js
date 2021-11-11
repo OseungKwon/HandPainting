ad76214a4fe5f71b6826194eb4425d27cc0004fe
OseungKwon <00osos@naver.com>
2021-11-11 17:57:17 +0900

    디렉터리 구조 수정

--- sketch.js
+++ sketch.js
@@ -1,85 +1,143 @@
-let canvas;
+let img = [];
+let images = [];
+let off = 300;
+let area;
+let spring = 0.05;
+let r = 700; //r값을 조절하면 화면 중심으로부터 튕기는 거리 조절
+function preload() {
+  for (let i = 0; i < 11; i++) {
+    img[i] = loadImage("img/" + i + ".png");
+  }
+}
 
-let sketch = function (p) {
-  p.setup = function () {
-    canvas = p.createCanvas(1920, 1080);
-    canvas.id("canvas");
+function setup() {
+  createCanvas(1920, 1080);
+  for (let i = 0; i < 6; i++) {
+    images[i] = new Img(
+      i,
+      random(0, width / 4),
+      random(0, height),
+      random(-1, 1),
+      random(-0.05, 0.05),
+      random(-1, 1),
+      random(-1, 1)
+    );
+  }
+  for (let i = 6; i < img.length; i++) {
+    images[i] = new Img(
+      i,
+      random((width / 4) * 3, width),
+      random(0, height),
+      random(-1, 1),
+      random(-0.05, 0.05),
+      random(-2, 2),
+      random(-2, 2)
+    );
+  }
+  area = new Area(width / 2, height / 2, r, r);
+}
 
-    p.colorMode(p.HSB);
-  };
+function draw() {
+  imageMode(CORNER);
+  background(0);
 
-  p.draw = function () {
-    p.clear();
-    if (detections != undefined) {
-      if (detections.multiHandLandmarks != undefined) {
-        //p.drawHands();
-        // p.drawParts();
+  for (let i = 0; i < img.length; i++) {
+    images[i].draw();
+    images[i].rotate();
+    images[i].move();
+    images[i].collide(area);
+  }
+  //area.draw(); //이 줄 없애면 가운데 원은 안 보이게 됩니다.
+  background(0, 100);
+}
 
-        p.drawLines([0, 5, 9, 13, 17, 0]); //palm
-        p.drawLines([0, 1, 2, 3, 4]); //thumb
-        p.drawLines([5, 6, 7, 8]); //index finger
-        p.drawLines([9, 10, 11, 12]); //middle finger
-        p.drawLines([13, 14, 15, 16]); //ring finger
-        p.drawLines([17, 18, 19, 20]); //pinky
+class Img {
+  constructor(i, x, y, angle, inc, xSpeed, ySpeed) {
+    this.i = i;
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.inc = inc;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.w = img[this.i].width;
+    this.h = img[this.i].height;
+  }
 
-        p.drawLandmarks([0, 1], 0); //palm base
-        p.drawLandmarks([1, 5], 60); //thumb
-        p.drawLandmarks([5, 9], 120); //index finger
-        p.drawLandmarks([9, 13], 180); //middle finger
-        p.drawLandmarks([13, 17], 240); //ring finger
-        p.drawLandmarks([17, 21], 300); //pinky
-      }
+  draw() {
+    imageMode(CENTER);
+    push();
+    fill(255);
+    translate(this.x, this.y);
+    rotate(this.angle);
+    image(img[this.i], 0, 0, this.w, this.h);
+    pop();
+  }
+  rotate() {
+    this.angle += this.inc;
+  }
+  reduce() {
+    this.w = lerp(this.w, img[this.i].width * 0.5, 0.01);
+    this.h = lerp(this.h, img[this.i].height * 0.5, 0.01);
+  }
+
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    if (this.x < this.w / 2) {
+      this.x = this.w / 2;
+      this.xSpeed = this.xSpeed * -1;
     }
-  };
-
-  p.drawHands = function () {
-    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
-      for (let j = 0; j < detections.multiHandLandmarks[i].length; j++) {
-        let x = detections.multiHandLandmarks[i][j].x * p.width;
-        let y = detections.multiHandLandmarks[i][j].y * p.height;
-        let z = detections.multiHandLandmarks[i][j].z;
-        // p.strokeWeight(0);
-        // p.textFont('Helvetica Neue');
-        // p.text(j, x, y);
-        p.stroke(255);
-        p.strokeWeight(10);
-        p.point(x, y);
-      }
+    if (this.x > width - this.w / 2) {
+      this.x = width - this.w / 2;
+      this.xSpeed = this.xSpeed * -1;
     }
-  };
-
-  p.drawLandmarks = function (indexArray, hue) {
-    p.noFill();
-    p.strokeWeight(9);
-    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
-      for (let j = indexArray[0]; j < indexArray[1]; j++) {
-        let x = detections.multiHandLandmarks[i][j].x * p.width;
-        let y = detections.multiHandLandmarks[i][j].y * p.height;
-        // let z = detections.multiHandLandmarks[i][j].z;
-        p.stroke(hue, 0, 255);
-        p.point(x, y);
-        // find left hand
-        //if (j === 4 && i === 0) p.rect(x, y, 50, 50);
-      }
+    if (this.y < this.h / 2) {
+      this.y = this.h / 2;
+      this.ySpeed = this.ySpeed * -1;
     }
-  };
-
-  p.drawLines = function (index) {
-    p.stroke(0, 0, 80);
-    p.strokeWeight(5);
-    for (let i = 0; i < detections.multiHandLandmarks.length; i++) {
-      for (let j = 0; j < index.length - 1; j++) {
-        let x = detections.multiHandLandmarks[i][index[j]].x * p.width;
-        let y = detections.multiHandLandmarks[i][index[j]].y * p.height;
-        // let z = detections.multiHandLandmarks[i][index[j]].z;
-
-        let _x = detections.multiHandLandmarks[i][index[j + 1]].x * p.width;
-        let _y = detections.multiHandLandmarks[i][index[j + 1]].y * p.height;
-        // let _z = detections.multiHandLandmarks[i][index[j+1]].z;
-        p.line(x, y, _x, _y);
-      }
+    if (this.y > height - this.h / 2) {
+      this.y = height - this.h / 2;
+      this.ySpeed = this.ySpeed * -1;
     }
-  };
-};
+    //     let d=dist(width/2,height/2,this.x,this.y);
+    //     if(d<off){
+    //       this.xSpeed=this.xSpeed*-1;
+    //       this.ySpeed=this.ySpeed*-1;
+    //     }
+  }
 
-let myp5 = new p5(sketch);
+  collide(other) {
+    let dx = other.x - this.x;
+    let dy = other.y - this.y;
+    let distance = sqrt(dx * dx + dy * dy);
+    let minDist;
+    if (this.w > this.h) {
+      minDist = other.r / 2 + this.w / 2;
+    } else {
+      minDist = other.r / 2 + this.h / 2;
+    }
+    if (distance < minDist) {
+      let angle = atan2(dy, dx);
+      let targetX = this.x + cos(angle) * minDist;
+      let targetY = this.y + sin(angle) * minDist;
+      let ax = (targetX - other.x) * spring;
+      let ay = (targetY - other.y) * spring;
+
+      this.xSpeed -= ax;
+      this.ySpeed -= ay;
+    }
+  }
+}
+
+class Area {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+  draw() {
+    circle(this.x, this.y, this.r);
+  }
+}
